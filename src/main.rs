aaee61f1783f18196323c685d551d3047bc7113c
randycanegaly <canegaly@gmail.com>
2019-12-26 17:31:59 -0500

    completed with quit working correctly

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,28 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1,101);
-    println!("The secret number is: {}", secret_number);
+    //println!("The secret number is: {}", secret_number);
 
     loop {
         println!("Please input your guess.");
-
 
         let mut guess = String::new();
 
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line.");
 
-            let guess: u32 = guess.trim().parse().expect("Please type a number.");
+            if guess.trim() == "quit" {
+                //println!("I see quit!");
+                break;
+            } else {
+                println!("guess is {}", guess);
+            }
+
+            let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                //Err("quit") => break,
+                Err(_) => continue
+            };
 
             println!("You guessed: {}", guess);
 
